6482c6acd46fbb16ab3446ad9b50b0eb3e6606cd
Your Name <shubhamgupta190496@gmail.com>
2016-10-23 13:19:45 +0530

    requirements added

--- spoj_follower/spoj_follower/urls.py
+++ spoj_follower/spoj_follower/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^spoj_follower/', include('follower.urls')),
+    url(r'^', include('follower.urls')),
     url(r'^admin/', admin.site.urls),
 ]
