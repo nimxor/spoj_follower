781633313a16fc7e5064a97ee2369f52270097e7
Your Name <shubhamgupta190496@gmail.com>
2016-12-09 14:13:19 +0530

    changes done

--- spoj_follower/spoj_follower/settings.py
+++ spoj_follower/spoj_follower/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '!pfn)t!i9z6)_mpujx2=6)2%bty91h%9zw^u1*-lz+w-bj%inc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['spoj-follower.herokuapp.com'] 
 
 
 # Application definition
@@ -124,8 +124,6 @@ STATIC_URL = '/static/'
 DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-ALLOWED_HOSTS = ['*']
 
 DEBUG = False
 
